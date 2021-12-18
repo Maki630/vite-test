41518a3cde10f6e6fea01256191ba17b750ac32f
司若妍 <maki@siruoyandeMacBook-Pro.local>
2021-12-18 11:58:53 +0800

    add delete

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,5 +1,7 @@
 import React, { useState }from 'react';
 import { Input, Button } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+import { cloneDeep } from 'lodash-es'
 
 const TodoItem = () => {
   const [todoList, setsTodoList] = useState([])
@@ -16,13 +18,20 @@ const TodoItem = () => {
     setTemplate('')
   }
 
+  const deleteItem = (index) => {
+    console.log(index)
+    const newList = cloneDeep(todoList)
+    const list = newList.filter((item, i) => i !== index)
+    setsTodoList(list)
+  }
+
   return (
       <div className="todoItem">
           <div className="addItem"><Input onChange={(e)=> enterContent(e.target.value)} value={template} /><Button type="primary" onClick={() => addItem()}>Add</Button></div>
           <ul className='list'>
-            {todoList.map(item => {
+            {todoList.map((item, index) => {
               return (
-                <li>{item}</li>
+                <li>{item}<div className='delete' onClick={() => deleteItem(index)}><CloseOutlined style={{ marginLeft: '5px', fontSize: '16px', color: '#08c' }} /></div></li>
               )
             })}
           </ul>
